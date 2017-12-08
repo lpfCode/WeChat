a562df5ebf58dd61581b0ce1d707ee71ae0b35ac
lpfCode <lpf_student@163.com>
2017-12-08 11:32:13 +0800

    新建分支配置路由controller以及中间件

---
+++ app/Http/Controllers/WeChatController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yonghu
+ * Date: 2017/12/8
+ * Time: 上午11:14
+ */
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Support\Facades\Log;
+use PharIo\Manifest\Application;
+
+class WeChatController extends Controller {
+
+    public function service(){
+
+        Log::info('请求已收到');
+        $wechat = app('wx');
+        $wechat->server->setMessageHandler(function ($message){
+
+            return "已经连接";
+        });
+        Log::info('返回的数据');
+        return $wechat->server->serve();
+    }
+    //使用容器自动注入
+    public function demo(Application $wechat){
+        //$wechat 则为容器中 EasyWeChat\Foundation\Application的实例
+    }
+}
