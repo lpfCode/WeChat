df287f7067d037208acaa61f81f2241c1e747bf9
lpfCode <lpf_student@163.com>
2017-12-08 15:26:59 +0800

    serve()

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -23,8 +23,9 @@ class WeChatController extends Controller {
 
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
-        $wechat = app('wechat');
+        $wechat = new Application();
         $wechat->server->setMessageHandler(function($message){
+            
             return "欢迎关注 overtrue！";
         });
 
