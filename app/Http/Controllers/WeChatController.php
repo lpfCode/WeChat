d7f5b64fba2fa6936a84e8eeaafb30a3b2881710
lpfCode <lpf_student@163.com>
2017-12-08 15:14:39 +0800

    serve()

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -10,20 +10,31 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Support\Facades\Log;
-use PharIo\Manifest\Application;
+use EasyWeChat\Foundation\Application;
 
 class WeChatController extends Controller {
 
-    public function service(){
-        if(isset($_GET['echostr'])){
-            //laravel打印 调试很有用
-            Log::info($_GET);
-            return $_GET['echostr'];
-            exit;
-        }
+    public function serve(){
+//        if(isset($_GET['echostr'])){
+//            //laravel打印 调试很有用
+//            Log::info($_GET);
+//            return $_GET['echostr'];
+//            exit;
+
+        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
+        $wechat = app('wechat');
+        $wechat->server->setMessageHandler(function($message){
+            return "欢迎关注 overtrue！";
+        });
+
+        Log::info('return response.');
+
+        return $wechat->server->serve();
     }
     //使用容器自动注入
     public function demo(Application $wechat){
-        //$wechat 则为容器中 EasyWeChat\Foundation\Application的实例
+
+
     }
 }
