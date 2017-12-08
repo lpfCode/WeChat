69c7c1b841a0fbf365462bb7dbedbed1bef7be6e
lpfCode <lpf_student@163.com>
2017-12-08 15:39:10 +0800

    io

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -11,6 +11,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Log;
 use EasyWeChat\Foundation\Application;
+use function Psy\debug;
 
 class WeChatController extends Controller {
 
@@ -23,9 +24,16 @@ class WeChatController extends Controller {
 
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
-        $wechat = new Application();
+        $options = [
+
+            'debug'  => true,
+            'app_id' => 'wx3e2968fb448ca1d4',
+            'secret' => '2d839eae8a64fe08d2d6a752a37c3d9a',
+            'token'  => 'weixin'
+        ];
+        $wechat = new Application($options);
         $wechat->server->setMessageHandler(function($message){
-            
+
             return "欢迎关注 overtrue！";
         });
 
